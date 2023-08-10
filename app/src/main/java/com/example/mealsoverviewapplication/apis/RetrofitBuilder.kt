12522eef4a28d307b2d06e29d0b9ee5903ff28c0
HiAnhhanhh <hoanganh124356@gmail.com>
2023-08-10 09:29:08 +0700

    Commit

--- app/src/main/java/com/example/mealsoverviewapplication/apis/RetrofitBuilder.kt
+++ app/src/main/java/com/example/mealsoverviewapplication/apis/RetrofitBuilder.kt
@@ -4,8 +4,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-
-
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BaseUrl.baseUrl)
